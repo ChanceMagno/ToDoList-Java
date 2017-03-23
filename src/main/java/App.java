11f7e29ca8f251ea3749074f0bb07f6522a25fb2
ChanceMagno <chancemagno@gmail.com>
2017-03-23 15:06:38 -0700

    completed TODOList

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,21 +18,85 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/tasks-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", ToDo.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("description");
+      ToDo newTask = new ToDo(description);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      ToDo task = ToDo.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/category-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/categories", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      model.put("template", "templates/category-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id/tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("template", "templates/category-tasks-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
-      ArrayList<ToDo> tasks = request.session().attribute("tasks");
-      if (tasks == null) {
-        tasks = new ArrayList<ToDo>();
-        request.session().attribute("tasks", tasks);
-      }
+      Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
 
       String description = request.queryParams("description");
       ToDo newTask = new ToDo(description);
-      tasks.add(newTask);
 
-      model.put("template", "templates/success.vtl");
+      category.addTask(newTask);
+
+      model.put("category", category);
+      model.put("template", "templates/category-task-success.vtl");
       return new ModelAndView(model, layout);
-      }, new VelocityTemplateEngine());
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class ToDoTest {
   @Test
@@ -7,9 +8,51 @@ public class ToDoTest {
     ToDo myTask = new ToDo("Mow the lawn");
     assertEquals(true, myTask instanceof ToDo);
   }
+
   @Test
   public void ToDoItem_createsDescription_String() {
     ToDo myTask = new ToDo("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    ToDo myTask = new ToDo("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    ToDo myTask = new ToDo("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTask_true() {
+    ToDo firstTask = new ToDo("Mow the lawn");
+    ToDo secondTask = new ToDo("Buy groceries");
+    assertEquals(true, ToDo.all().contains(firstTask));
+    assertEquals(true, ToDo.all().contains(secondTask));
+  }
+
+  @Test
+  public void clear_emptiesAllTaskFromArrayList_0() {
+    ToDo myTask = new ToDo("Mow the lawn");
+    ToDo.clear();
+    assertEquals(0, ToDo.all().size());
+  }
+
+  @Test
+  public void getId_todoInstantiatesWithAnID_1() {
+    ToDo.clear();
+    ToDo myTask = new ToDo("Mow the lawn");
+    assertEquals(1, myTask.getId());
+  }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    ToDo firstTask = new ToDo("Mow the lawn");
+    ToDo secondTask = new ToDo("Buy groceries");
+    assertEquals(ToDo.find(secondTask.getId()), secondTask);
   }
 }
